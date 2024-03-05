c32f0f84e1c5488dc87720fe0ce1716f6e81035e
gn <115848472+g-nbo@users.noreply.github.com>
2024-03-05 11:49:31 -0500

    Create UI for Rock Paper Scissors game.

--- rps.js
+++ rps.js
@@ -25,61 +25,104 @@ function playRound(playerSelection, computerSelection) {
     console.log('Player Chose: ' + playerSelection + ' \nComputer Chose: ' + computerSelection);
     if(playerSelection === 'rock' && computerSelection == 'scissors') {
         playerWins += 1;
-        return console.log('You Win! Rock beats Scissors!');
+        roundsPlayed += 1;
+        return //console.log('You Win! Rock beats Scissors!');
     }
     else if(playerSelection === 'paper' && computerSelection == 'rock') {
         playerWins += 1;
-        return  console.log('You Win! Paper beats Rock!');
+        roundsPlayed += 1;
+        return  //console.log('You Win! Paper beats Rock!');
     }
     else if(playerSelection === 'scissors' && computerSelection == 'paper') {
         playerWins += 1;
-        return  console.log('You Win! Scissors beats Paper!');
+        roundsPlayed += 1;
+        return  //console.log('You Win! Scissors beats Paper!');
     }
     else if(playerSelection === computerSelection) {
-        return console.log("It's a Draw!");
+        return //console.log("It's a Draw!");
     }
     else if(playerSelection === 'scissors' && computerSelection == 'rock') {
         computerWins += 1;
-        return  console.log('You Lose! Rock beats Scissors!');
+        roundsPlayed += 1;
+        return  //console.log('You Lose! Rock beats Scissors!');
     }
     else if(playerSelection === 'rock' && computerSelection == 'paper') {
         computerWins += 1; 
-        return console.log('You Lose! Rock beats Scissors!');
+        roundsPlayed += 1;
+        return //console.log('You Lose! Rock beats Scissors!');
         
     }
     else if(playerSelection === 'paper' && computerSelection == 'scissors') {
         computerWins += 1;
-        return  console.log('You Lose! Scissors beats Paper!');
+        roundsPlayed += 1;
+        return  //console.log('You Lose! Scissors beats Paper!');
     }
 }
+
+
+
 
 function playGame() {
 
-    for(i = 0; i < 5; i++)
-    {
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("Rock Paper or Scissors?: ").toLowerCase();
-        console.log('Score: P1 (' + playerWins + ') CPU (' + computerWins + ')');
-        playRound(playerSelection, computerSelection);
+    function checkWin() {
+        if(roundsPlayed >= 5 && playerWins >= 5) {
+            return announceWin.textContent = "Player Wins!";
+        }
+        else if(roundsPlayed >= 5 && computerWins >= 5) {
+            return announceWin.textContent = "Computer Wins!";
+        }
+        else
+            return;
+        
     }
     
-    console.log('Final Score: P1 (' + playerWins + ') CPU (' + computerWins + ')');
+    
+    const rock = document.querySelector('#rock');
+    rock.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = 'rock';
+    playRound(playerSelection, computerSelection);
+    score.textContent = ('Score: P1 (' + playerWins + ') CPU (' + computerWins + ')');
+    checkWin()
+    })
 
-    if(playerWins > computerWins) {
-        return "Player Wins!";
-    }
-    else if(computerWins > playerWins) {
-        return "Computer Wins!";
-    }
-    else {
-        return "Draw!";
-    }
+    const paper = document.querySelector('#paper');
+    paper.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = 'paper';
+    playRound(playerSelection, computerSelection);
+    score.textContent = ('Score: P1 (' + playerWins + ') CPU (' + computerWins + ')');
+    checkWin()
+    })
+
+    const scissors = document.querySelector('#scissors');
+    scissors.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = 'scissors';
+    playRound(playerSelection, computerSelection);
+    score.textContent = ('Score: P1 (' + playerWins + ') CPU (' + computerWins + ')');
+    checkWin()
+    })
+
+    const results = document.querySelector('#results');
+
+    const score = document.createElement('p');
+    score.classList.add('score');
+    score.textContent = ('Score: P1 (' + playerWins + ') CPU (' + computerWins + ')');
+
+    const announceWin = document.createElement('p');
+    announceWin.classList.add('announceWin');
+
     
+    
+
+    results.appendChild(score);
+    results.appendChild(announceWin);
 }
+    
 
-//const computerSelection = getComputerChoice();
-//const playerSelection = prompt("Rock Paper or Scissors?: ").toLowerCase();
 let computerWins = 0;
 let playerWins = 0;
+let roundsPlayed = 0;
 
 console.log(playGame());
